a0986ec20727772d93d207a8a9b5752dc6e41788
colaru <colaru@gmail.com>
2013-06-21 16:10:17 +0300

    Some WS tests.

--- src/test/java/com/openwager/rtp/test/integration/java/BasicIntegrationTest.java
+++ src/test/java/com/openwager/rtp/test/integration/java/BasicIntegrationTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 import org.vertx.java.core.AsyncResult;
 import org.vertx.java.core.AsyncResultHandler;
 import org.vertx.java.core.Handler;
-import org.vertx.java.core.http.HttpClientResponse;
-import org.vertx.java.core.http.HttpServer;
-import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.core.http.*;
 import org.vertx.testtools.TestVerticle;
 
 import static org.vertx.testtools.VertxAssert.*;
@@ -16,60 +14,82 @@ import static org.vertx.testtools.VertxAssert.*;
  */
 public class BasicIntegrationTest extends TestVerticle {
 
-  @Test
+    private static final int CONNS = 100;
+    int connectCount = 0;
+
+    @Test
   /*
   This demonstrates using the Vert.x API from within a test.
    */
-  public void testHTTP() {
-    // Create an HTTP server which just sends back OK response immediately
-    vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
-      public void handle(HttpServerRequest req) {
-        req.response().end();
-      }
-    }).listen(8181, new AsyncResultHandler<HttpServer>() {
-      @Override
-      public void handle(AsyncResult<HttpServer> asyncResult) {
-        assertTrue(asyncResult.succeeded());
-        // The server is listening so send an HTTP request
-        vertx.createHttpClient().setPort(8181).getNow("/",new Handler<HttpClientResponse>() {
-          @Override
-          public void handle(HttpClientResponse resp) {
-            assertEquals(200, resp.statusCode());
+    public void testHTTP() {
+        // Create an HTTP server which just sends back OK response immediately
+        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                req.response().end();
+            }
+        }).listen(8181, new AsyncResultHandler<HttpServer>() {
+            @Override
+            public void handle(AsyncResult<HttpServer> asyncResult) {
+                assertTrue(asyncResult.succeeded());
+                // The server is listening so send an HTTP request
+                vertx.createHttpClient().setPort(8181).getNow("/", new Handler<HttpClientResponse>() {
+                    @Override
+                    public void handle(HttpClientResponse resp) {
+                        assertEquals(200, resp.statusCode());
             /*
             If we get here, the test is complete
             You must always call `testComplete()` at the end. Remember that testing is *asynchronous* so
             we cannot assume the test is complete by the time the test method has finished executing like
             in standard synchronous tests
             */
-            testComplete();
-          }
+                        testComplete();
+                    }
+                });
+            }
         });
-      }
-    });
-  }
+    }
 
-  @Test
+    @Test
   /*
   This test deploys some arbitrary verticle - note that the call to testComplete() is inside the Verticle `SomeVerticle`
    */
-  public void testDeployArbitraryVerticle() {
-    assertEquals("bar", "bar");
-    container.deployVerticle(SomeVerticle.class.getName());
-  }
+    public void testDeployArbitraryVerticle() {
+        assertEquals("bar", "bar");
+        container.deployVerticle(SomeVerticle.class.getName());
+    }
 
-  @Test
-  public void testCompleteOnTimer() {
-    vertx.setTimer(1000, new Handler<Long>() {
-      @Override
-      public void handle(Long timerID) {
-        assertNotNull(timerID);
+    @Test
+    public void testDeployWebSoketServerVerticle() {
+        assertEquals("bar", "bar");
+        container.deployVerticle(WebSoketServer.class.getName(), 2);
 
-        // This demonstrates how tests are asynchronous - the timer does not fire until 1 second later -
-        // which is almost certainly after the test method has completed.
-        testComplete();
-      }
-    });
-  }
+        System.out.println("Starting perf client");
+        HttpClient client = vertx.createHttpClient().setPort(8080).setHost("localhost").setMaxPoolSize(CONNS);
+        for (int i = 0; i < CONNS; i++) {
+            System.out.println("connecting ws");
+            client.connectWebsocket("/someuri", new Handler<WebSocket>() {
+                public void handle(WebSocket ws) {
+                    System.out.println("ws connected: " + ++connectCount);
+                    testComplete();
+                }
+            });
+        }
+
+    }
+
+    @Test
+    public void testCompleteOnTimer() {
+        vertx.setTimer(1000, new Handler<Long>() {
+            @Override
+            public void handle(Long timerID) {
+                assertNotNull(timerID);
+
+                // This demonstrates how tests are asynchronous - the timer does not fire until 1 second later -
+                // which is almost certainly after the test method has completed.
+                testComplete();
+            }
+        });
+    }
 
 
 }
---
+++ src/test/java/com/openwager/rtp/test/integration/java/WebSoketServer.java
@@ -0,0 +1,20 @@
+package com.openwager.rtp.test.integration.java;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.ServerWebSocket;
+import org.vertx.java.platform.Verticle;
+
+public class WebSoketServer extends Verticle {
+    private static final int BUFF_SIZE = 32 * 1024;
+
+    int count = 0;
+
+    public void start() {
+        vertx.createHttpServer().setReceiveBufferSize(BUFF_SIZE).setSendBufferSize(BUFF_SIZE).
+                websocketHandler(new Handler<ServerWebSocket>() {
+                    public void handle(ServerWebSocket ws) {
+                        System.out.println("connected " + ++count);
+                    }
+                }).listen(8080, "localhost");
+    }
+}
