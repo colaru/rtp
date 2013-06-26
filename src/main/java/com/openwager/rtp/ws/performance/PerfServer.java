1c4cbafc1814c626b58d18999764316c23da31db
colaru <colaru@gmail.com>
2013-06-26 12:02:44 +0300

    Event bus and web sockets tests from vert,x examples.

---
+++ src/main/java/com/openwager/rtp/ws/performance/PerfServer.java
@@ -0,0 +1,23 @@
+package com.openwager.rtp.ws.performance;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.ServerWebSocket;
+import org.vertx.java.core.streams.Pump;
+import org.vertx.java.platform.Verticle;
+
+public class PerfServer extends Verticle {
+
+  private static final int BUFF_SIZE = 32 * 1024;
+  int count = 0;
+
+  public void start() {
+    vertx.createHttpServer().setReceiveBufferSize(BUFF_SIZE).setSendBufferSize(BUFF_SIZE).setAcceptBacklog(32000).
+        websocketHandler(new Handler<ServerWebSocket>() {
+
+      public void handle(ServerWebSocket ws) {
+        System.out.println("connected " +  ++count);
+        Pump.createPump(ws, ws, BUFF_SIZE).start();
+      }
+    }).listen(8080, "localhost");
+  }
+}
--- src/test/java/com/openwager/rtp/test/integration/java/BasicIntegrationTest.java
+++ src/test/java/com/openwager/rtp/test/integration/java/BasicIntegrationTest.java
@@ -85,7 +85,7 @@ public class BasicIntegrationTest extends TestVerticle {
                         }
                     });
 
-//                    testComplete();
+                    testComplete();
                 }
             });
         }
