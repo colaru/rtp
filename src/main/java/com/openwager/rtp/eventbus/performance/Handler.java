bbd0d94a46645b2e3d8dd5f2b2427e77f7886a13
colaru <colaru@gmail.com>
2013-07-01 12:07:58 +0300

    Add fibonacci method to be used in tests!

--- src/main/java/com/openwager/rtp/eventbus/performance/Handler.java
+++ src/main/java/com/openwager/rtp/eventbus/performance/Handler.java
@@ -23,6 +23,7 @@ public class Handler extends Verticle {
             @Override
             public void handle(Message<String> message) {
 
+//                calculateFibonacciSeries(200);
 
                 received++;
                 if (received == batchSize)
@@ -47,6 +48,29 @@ public class Handler extends Verticle {
                 }
             }
         });
+    }
+
+    private String calculateFibonacciSeries(int limit) {
+        String response = "";
+        long[] series = new long[limit];
+
+        //create first 2 series elements
+        series[0] = 0;
+        series[1] = 1;
+
+        //create the Fibonacci series and store it in an array
+        for(int i=2; i < limit; i++){
+            series[i] = series[i-1] + series[i-2];
+        }
+
+        //print the Fibonacci series numbers
+
+        response += "Fibonacci Series upto " + limit;
+        for(int i=0; i< limit; i++){
+            response += series[i] + " ";
+        }
+
+        return response;
     }
 }
 
--- src/main/java/com/openwager/rtp/ws/performance/PerfServer.java
+++ src/main/java/com/openwager/rtp/ws/performance/PerfServer.java
@@ -19,8 +19,33 @@ public class PerfServer extends Verticle {
 
       public void handle(ServerWebSocket ws) {
         System.out.println("connected " +  ++count);
+
+//        calculateFibonacciSeries(200);
         Pump.createPump(ws, ws, BUFF_SIZE).start();
       }
     }).listen(8080, "localhost");
   }
+
+    private String calculateFibonacciSeries(int limit) {
+        String response = "";
+        long[] series = new long[limit];
+
+        //create first 2 series elements
+        series[0] = 0;
+        series[1] = 1;
+
+        //create the Fibonacci series and store it in an array
+        for(int i=2; i < limit; i++){
+            series[i] = series[i-1] + series[i-2];
+        }
+
+        //print the Fibonacci series numbers
+
+        response += "Fibonacci Series upto " + limit;
+        for(int i=0; i< limit; i++){
+            response += series[i] + " ";
+        }
+
+        return response;
+    }
 }
